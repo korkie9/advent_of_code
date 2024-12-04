f9cf824ddadaf6639e74d1c2657f92869183d877
Justin Korkie <justinjkorkie@gmail.com>
2024-12-04 22:59:00 +0200

    day 4 puzzle 2 2024

--- 2024/day_4/puzzle_2.php
+++ 2024/day_4/puzzle_2.php
@@ -10,7 +10,34 @@ class Puzzle2 implements Solution
   public static function solve(): int
   {
 
-    /*$input = lib\GetInputArr('../day_3/input.txt');*/
-    return 0;
+    $input = lib\GetInputArr('input.txt');
+    $multiDArray = array_map(function ($line) {
+      return str_split($line);
+    }, $input);
+
+    $ans = self::getXmas($multiDArray);
+
+    return $ans;
+  }
+
+
+  private static function getXmas(array $arr): int
+  {
+    $count = 0;
+    foreach ($arr as $yindex => $yline) {
+      foreach ($yline as $xindex => $xline) {
+        $topLeftExists = isset($arr[$yindex - 1][$xindex - 1]);
+        $topRightExists = isset($arr[$yindex - 1][$xindex + 1]);
+        $bottomLeftExists = isset($arr[$yindex + 1][$xindex - 1]);
+        $bottomRightExists = isset($arr[$yindex + 1][$xindex + 1]);
+        if (!$topLeftExists || !$topRightExists || !$bottomLeftExists || !$bottomRightExists) {
+          continue;
+        }
+        $backslash = $arr[$yindex - 1][$xindex - 1] . $arr[$yindex][$xindex] . $arr[$yindex + 1][$xindex + 1];
+        $forwardslash = $arr[$yindex + 1][$xindex - 1] . $arr[$yindex][$xindex] . $arr[$yindex - 1][$xindex + 1];
+        if (($backslash == "MAS" || $backslash == "SAM") && ($forwardslash == "MAS" || $forwardslash == "SAM")) $count++;
+      }
+    }
+    return $count;
   }
 }
