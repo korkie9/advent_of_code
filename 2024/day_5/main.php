0a05415075144119967cab4420b730f7a55a73f6
Justin Korkie <justinjkorkie@gmail.com>
2024-12-06 20:47:30 +0200

    puzzle 2 day 5 2024

--- 2024/day_5/main.php
+++ 2024/day_5/main.php
@@ -3,4 +3,4 @@ require_once 'puzzle_1.php';
 require_once 'puzzle_2.php';
 
 echo "Solution 1: " . Puzzle1::solve() . "\n";
-/*echo "Solution 2: " . Puzzle2::solve() . "\n";*/
+echo "Solution 2: " . Puzzle2::solve() . "\n";
--- 2024/day_5/puzzle_2.php
+++ 2024/day_5/puzzle_2.php
@@ -1,4 +1,3 @@
-
 <?php
 
 
@@ -10,15 +9,93 @@ class Puzzle2 implements Solution
 
   public static function solve(): int
   {
-
-    $datasetFileString = '../day_5/dataset_test_file.txt';
-    $rulesetFileString = '../day_5/ruleset_test_file.txt';
-    /*$datasetFileString = '../day_5/dataset_file.txt';*/
-    /*$rulesetFileString = '../day_5/ruleset_file.txt';*/
-
+    /*$datasetFileString = '../day_5/dataset_test_file.txt';*/
+    /*$rulesetFileString = '../day_5/ruleset_test_file.txt';*/
+    $datasetFileString = '../day_5/dataset_file.txt';
+    $rulesetFileString = '../day_5/ruleset_file.txt';
     $dataset_input = lib\GetInputArr($datasetFileString);
     $ruleset_input = lib\GetInputArr($rulesetFileString);
 
-    return 0;
+    $ruleset = array_map(function ($val) {
+      return explode("|", $val);
+    }, $ruleset_input);
+    $dataset = array_map(function ($val) {
+      return explode(",", $val);
+    }, $dataset_input);
+
+    $ans = 0;
+
+    foreach ($dataset as $index => $data_arr) {
+      if (!self::arraryIsValid($data_arr, $ruleset)) {
+        $sorted_arr = self::sortArr($data_arr, $ruleset);
+
+        $mid = floor(sizeof($sorted_arr) / 2);
+        $ans += $sorted_arr[$mid];
+      }
+    }
+
+
+
+    return $ans;
+  }
+
+  private static function arraryIsValid(array $eval_arr, array $ruleset): bool
+  {
+    foreach ($eval_arr as $eval_index => $val) {
+      for ($other_index = $eval_index; $other_index < sizeof($eval_arr); $other_index++) {
+        if ($eval_index == $other_index) continue;
+        //Check ruleset with the two indexes from the arrays 
+        foreach ($ruleset as $rule_index => $rule_arr) {
+
+          if (
+            $eval_index > $other_index
+            && $eval_arr[$eval_index] == $rule_arr[0]
+            && $eval_arr[$other_index] == $rule_arr[1]
+          ) return false;
+          if (
+            $eval_index < $other_index
+            && $eval_arr[$eval_index] == $rule_arr[1]
+            && $eval_arr[$other_index] == $rule_arr[0]
+          ) return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private static function sortArr(array $eval_arr, array $ruleset): array
+  {
+    $tempArr = $eval_arr;
+    while (!self::arraryIsValid($eval_arr, $ruleset)) {
+
+
+      foreach ($eval_arr as $eval_index => $val) {
+        for ($other_index = 0; $other_index < sizeof($eval_arr); $other_index++) {
+          if ($eval_index == $other_index) continue; // TODO: Might need to delete this
+          //Check ruleset with the two indexes from the arrays 
+          foreach ($ruleset as $rule_index => $rule_arr) {
+
+            if (
+              $eval_index > $other_index
+              && $tempArr[$eval_index] == $rule_arr[0]
+              && $tempArr[$other_index] == $rule_arr[1]
+            ) {
+              [$tempArr[$eval_index], $tempArr[$other_index]] = [$tempArr[$other_index], $tempArr[$eval_index]];
+            };
+            if (
+              $eval_index < $other_index
+              && $tempArr[$eval_index] == $rule_arr[1]
+              && $tempArr[$other_index] == $rule_arr[0]
+            ) {
+              [$tempArr[$eval_index], $tempArr[$other_index]] = [$tempArr[$other_index], $tempArr[$eval_index]];
+            }
+          }
+        }
+      }
+
+
+      return $tempArr;
+    }
   }
 }
